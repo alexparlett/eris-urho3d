fc67c285312622dcb914f2c4960123cf96846cee
Alex Parlett <alexparlett@gmail.com>
2013-12-23 11:41:30 +0000

    [SW-2] Fixes the icons to be relative for building on different machines Added Git Version Stamping for all projects

---
+++ gitversion.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+cd %1
+git describe --long --tags --dirty --always > gitversion.tmp
+SET /p version= < gitversion.tmp
+DEL gitversion.tmp
+FOR /F "tokens=1-5 delims=.-" %%A IN ("%version%") DO (
+	SET MAJOR=%%A&SET MINOR=%%B&SET REVISION=%%C&SET BUILD=%%D&SET DIRTY=%%E
+)
+SET /a MINOR+=1
+IF DEFINED DIRTY GOTO DIRTYBUILD
+:CLEANBUILD 
+(ECHO #define FILEVER %MAJOR%,%MINOR%,%REVISION%,0)>Source/gitversion.h
+(ECHO #define PRODUCTVER %MAJOR%,%MINOR%,%REVISION%,0)>>Source/gitversion.h
+(ECHO #define FILEVERSTR %MAJOR%.%MINOR%.%REVISION%.%BUILD%)>>Source/gitversion.h
+(ECHO #define PRODUCTVERSTR %MAJOR%.%MINOR%.%REVISION%.%BUILD%)>>Source/gitversion.h
+(ECHO.)>>Source/gitversion.h
+(ECHO const char* version = %MAJOR%.%MINOR%.%REVISION%.%BUILD%)>>Source/gitversion.h
+GOTO CLEANUP
+:DIRTYBUILD
+(ECHO #define FILEVER %MAJOR%,%MINOR%,%REVISION%,0)>Source/gitversion.h
+(ECHO #define PRODUCTVER %MAJOR%,%MINOR%,%REVISION%,0)>>Source/gitversion.h
+(ECHO #define FILEVERSTR "%MAJOR%.%MINOR%.%REVISION%.%BUILD%-%DIRTY%")>>Source/gitversion.h
+(ECHO #define PRODUCTVERSTR "%MAJOR%.%MINOR%.%REVISION%.%BUILD%-%DIRTY%")>>Source/gitversion.h
+(ECHO.)>>Source/gitversion.h
+(ECHO const char* version = "%MAJOR%.%MINOR%.%REVISION%.%BUILD%-%DIRTY%")>>Source/gitversion.h
+GOTO CLEANUP
+:CLEANUP
+cd %2
